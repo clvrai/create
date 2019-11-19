94a8e65cf87d2058508e59fede7439fd92af2b28
ASzot <me@andrewszot.com>
2019-11-19 14:22:32 -0800

    Added some comments to evaluation script

--- examples/evaluation.py
+++ examples/evaluation.py
@@ -1,31 +1,33 @@
 import sys
+# import create game whatever way works best for you
 sys.path.insert(0, '.')
 sys.path.insert(0, '..')
-# import create game whatever way works best for you
-import create_game
-from create_game import CreateGameSettings
-import gym
-from create_game import UseSplit
 import numpy as np
-
-
+from create_game import UseSplit
+import gym
+from create_game import CreateGameSettings
 
 
 def gen_action_set(settings, tool_gen, allowed_actions, rng):
+    # Change the sampling strategy here. Our paper had a smart sampling
+    # strategy using k-means and our learned action embeddings.
     return np.random.choice(allowed_actions, settings.action_set_size, replace=False)
 
+
+# Settings used in our paper across all three environments. This is evaluating
+# on the test set of actions. The validation split of 0.5 uses half of the
+# evaluation split for testing, which is also what was used in our paper.
 use_settings = CreateGameSettings(
-    max_num_steps=5,
+    max_num_steps=30,
     action_random_sample=True,
     action_set_size=50,
     validation_ratio=0.5,
     split_type=UseSplit.TEST,
     action_sample_fn=gen_action_set)
 
-eval_lvls = ['CreateLevelPush-v0', 'CreateLevelNavigate-v0', 'CreateLevelObstacle-v0']
+eval_lvls = ['CreateLevelPush-v0',
+             'CreateLevelNavigate-v0', 'CreateLevelObstacle-v0']
 NUM_EVAL_EPISODES = 10
-
-
 
 for eval_lvl in eval_lvls:
     env = gym.make(eval_lvl)
@@ -38,6 +40,7 @@ for eval_lvl in eval_lvls:
         ep_reward = 0.0
         done = False
         while not done:
+            # Get the action from your policy
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
             ep_reward += reward
@@ -47,5 +50,3 @@ for eval_lvl in eval_lvls:
     avg_num_goal_hit = num_goal_hit / NUM_EVAL_EPISODES
 
     print('%s average goal hit (%%): %.4f%%' % (eval_lvl, avg_num_goal_hit))
-
-
